534728c157a449cd7daf3f3999e91e1891e39d63
vd9999 <varadandu0912@gmail.com>
2024-01-20 19:33:17 -0600

    first update

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send('Vara you successfully deployed APP on EKS using Github actionsl');
 });
 
 app.listen(PORT, HOST, () => {
